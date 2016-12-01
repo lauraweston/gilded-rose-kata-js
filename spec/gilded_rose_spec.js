d7365df737df562dc8e5be39c52be0b7fa857e12
Laura Weston <laura.weston@outlook.com>
2016-12-01 12:26:20 +0000

    Add additional test cases for Item tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,23 +2,31 @@ var gildedRose = require("../src/gilded_rose.js");
 var Item = gildedRose.item
 var updateQuality = gildedRose.updateQuality
 
-describe("Gilded Rose", function() {
-  
-  describe(Item, function() {
-    var item = new Item("foo", 0, 0);
-    it("has a name, sellIn value and quality value", function() {
-      expect(item.name).toEqual("foo");
-    });
-    it("has a name, sellIn value and quality value", function() {
-      expect(item.sellIn).toEqual(0);
-    });
-    it("has a name, sellIn value and quality value", function() {
-      expect(item.quality).toEqual(0);
+describe("Gilded Rose", function () {
+  var testCases = [
+    { name: "foo", sellIn: 0, quality: 0 },
+    { name: "bar", sellIn: 5, quality: 5 }
+  ];
+  testCases.forEach(function (testCase) {
+    describe(Item, function () {
+      beforeEach(function () {
+        item = new Item(testCase.name, testCase.sellIn, testCase.quality);
+      });
+
+      it("has a name", function () {
+        expect(item.name).toEqual(testCase.name);
+      });
+      it("has a sellIn value", function () {
+        expect(item.sellIn).toEqual(testCase.sellIn);
+      });
+      it("has a quality value", function () {
+        expect(item.quality).toEqual(testCase.quality);
+      });
     });
   });
 
-  it("should foo", function() {
-    var items = [ new Item("foo", 0, 0) ];
+  it("should foo", function () {
+    var items = [new Item("foo", 0, 0)];
     updateQuality();
     expect(items[0].name).toEqual("foo");
   });
