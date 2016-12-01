31e4e93a2e79dbf760793e0ed9f715cec757d3ce
Laura Weston <laura.weston@outlook.com>
2016-12-01 22:12:22 +0000

    Extract updateQuality logic to updateItemFunctions lookup

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,39 +14,43 @@ function decrementQuality(item) {
   item.quality -= 1;
 }
 
+var itemUpdateFunctions = {
+  "Aged Brie": function (item) {
+    if (item.quality < 50) {
+      incrementQuality(item);
+    }
+    item.sellIn -= 1;
+  },
+  "Backstage passes to a TAFKAL80ETC concert": function (item) {
+    if (item.quality < 50) {
+      incrementQuality(item);
+      if (item.sellIn < 11 && item.quality < 50) incrementQuality(item);
+      if (item.sellIn < 6 && item.quality < 50) incrementQuality(item);
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) item.quality = 0;
+  },
+  "Sulfuras, Hand of Ragnaros": function(item) {}
+};
+
+function updateStandardItem(item) {
+  if (item.quality > 0) {
+    decrementQuality(item);
+  }
+
+  item.sellIn -= 1;
+  if (item.sellIn < 0 && item.quality > 0) {
+    decrementQuality(item);
+  }
+}
+
 function updateQuality() {
   items.forEach(function (item) {
-    var brie = "Aged Brie";
-    var backstagePass = "Backstage passes to a TAFKAL80ETC concert";
-    var sulfuras = "Sulfuras, Hand of Ragnaros";
-    
-    if (item.name == brie) {
-      if (item.quality < 50) {
-        incrementQuality(item);
-      }
-      item.sellIn -= 1;
-      return;
-    }
-    if (item.name == backstagePass) {
-      if (item.quality < 50) {
-        incrementQuality(item);
-        if (item.sellIn < 11 && item.quality < 50) incrementQuality(item);
-        if (item.sellIn < 6 && item.quality < 50) incrementQuality(item);
-      }
-      item.sellIn -= 1;
-      if (item.sellIn < 0) item.quality = 0;
-      return;
-    }
-    if (item.name == sulfuras) {
-      return;
-    }
-    if (item.quality > 0) {
-      decrementQuality(item);
-    }
-
-    item.sellIn -= 1;
-    if (item.sellIn < 0 && item.quality > 0) {
-      decrementQuality(item);
+    var itemUpdateFunction = itemUpdateFunctions[item.name];
+    if (itemUpdateFunction) {
+      itemUpdateFunction(item);
+    } else {
+      updateStandardItem(item);
     }
   });
 }
