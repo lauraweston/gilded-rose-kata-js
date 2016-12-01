b41aa59c0797a357b6b530fa03aa1bfae4d781b2
Laura Weston <laura.weston@outlook.com>
2016-12-01 17:33:58 +0000

    Refactor updateQuality and extract incrementQuality and decrementQuality functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,49 +6,40 @@ function Item(name, sellIn, quality) {
 
 var items = []
 
+function incrementQuality(item) {
+  item.quality += 1;
+}
+
+function decrementQuality(item) {
+  item.quality -= 1;
+}
+
 function updateQuality() {
   items.forEach(function (item) {
-    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.quality > 0) {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1
-        }
-      }
-    } else {
+    if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
       if (item.quality < 50) {
-        item.quality = item.quality + 1
+        incrementQuality(item);
         if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
+          if (item.sellIn < 11 && item.quality < 50) incrementQuality(item);
+          if (item.sellIn < 6 && item.quality < 50) incrementQuality(item);
         }
       }
+    } else if (item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') {
+      decrementQuality(item);
     }
+
     if (item.name != 'Sulfuras, Hand of Ragnaros') {
       item.sellIn = item.sellIn - 1;
     }
     if (item.sellIn < 0) {
       if (item.name != 'Aged Brie') {
         if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1
-            }
-          }
+          if (item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') decrementQuality(item);
         } else {
           item.quality = item.quality - item.quality
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        if (item.quality < 50) incrementQuality(item);
       }
     }
   });
