a5da5509a79afc11de177432c2cf7f6210246d79
Laura Weston <laura.weston@outlook.com>
2016-12-01 22:36:43 +0000

    Checks min and max when incrementing/decrementing quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,15 +4,19 @@ function Item(name, sellIn, quality) {
   this.quality = quality;
 }
 
-Item.prototype.incrementQuality = function() {
-  this.quality++;
+Item.prototype.incrementQuality = function () {
+  if (this.quality < 50) {
+    this.quality++;
+  }
 }
 
-Item.prototype.decrementQuality = function() {
-  this.quality--;
+Item.prototype.decrementQuality = function () {
+  if (this.quality > 0) {
+    this.quality--;
+  }
 }
 
-Item.prototype.decrementSellIn = function() {
+Item.prototype.decrementSellIn = function () {
   this.sellIn--;
 }
 
@@ -20,29 +24,23 @@ var items = []
 
 var itemUpdateFunctions = {
   "Aged Brie": function (item) {
-    if (item.quality < 50) {
-      item.incrementQuality();
-    }
+    item.incrementQuality();
     item.decrementSellIn();
   },
   "Backstage passes to a TAFKAL80ETC concert": function (item) {
-    if (item.quality < 50) {
-      item.incrementQuality();
-      if (item.sellIn < 11 && item.quality < 50) item.incrementQuality();
-      if (item.sellIn < 6 && item.quality < 50) item.incrementQuality();
-    }
+    item.incrementQuality();
+    if (item.sellIn < 11) item.incrementQuality();
+    if (item.sellIn < 6) item.incrementQuality();
     item.decrementSellIn();
     if (item.sellIn < 0) item.quality = 0;
   },
-  "Sulfuras, Hand of Ragnaros": function(item) {}
+  "Sulfuras, Hand of Ragnaros": function (item) { }
 };
 
 function updateStandardItem(item) {
-  if (item.quality > 0) {
-    item.decrementQuality();
-  }
+  item.decrementQuality();
   item.decrementSellIn();
-  if (item.sellIn < 0 && item.quality > 0) {
+  if (item.sellIn < 0) {
     item.decrementQuality();
   }
 }
