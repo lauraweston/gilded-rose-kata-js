91ff479b95347394ce2aaeadbfc6117c9f0105aa
Laura Weston <laura.weston@outlook.com>
2016-12-01 12:08:14 +0000

    Add tests for Item properties

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,9 +3,22 @@ var Item = gildedRose.item
 var updateQuality = gildedRose.updateQuality
 
 describe("Gilded Rose", function() {
+  
+  describe(Item, function() {
+    var item = new Item("foo", 0, 0);
+    it("has a name, sellIn value and quality value", function() {
+      expect(item.name).toEqual("foo");
+    });
+    it("has a name, sellIn value and quality value", function() {
+      expect(item.sellIn).toEqual(0);
+    });
+    it("has a name, sellIn value and quality value", function() {
+      expect(item.quality).toEqual(0);
+    });
+  });
 
   it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+    var items = [ new Item("foo", 0, 0) ];
     updateQuality();
     expect(items[0].name).toEqual("foo");
   });
